f5e96fad29a3436e1afa27d62f530d6e2bde86f7
av-TOR <alexvoloshintor@yandex.ru>
2024-08-22 23:13:23 +0200

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as fm
+import true_math as am
+
+result1 = fm.divide(12, 0)
+result2 = am.divide(75, 0)
+result3 = fm.divide(60, 6)
+result4 = am.divide(1024, 20)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
